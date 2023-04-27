c511ae4685d3b8378a216f346ac3fafc794f56ac
raenier <raenierquiambao@gmail.com>
2023-04-27 13:46:20 +0800

    ADD UI
    commit 6ce889dbdd8b662deec70594d54ddeee2793b555
    Author: raenier <raenierquiambao@gmail.com>
    Date:   Thu Apr 27 13:39:57 2023 +0800
    
        Add final touches to the game
    
          - Add scoring
          - display result at the end
          - Add start and new game button
    
    commit 73a4480d86f76c0aa69ff0c90abfeaf53ec2c318
    Author: raenier <raenierquiambao@gmail.com>
    Date:   Wed Apr 26 16:12:03 2023 +0800
    
        Add Initial UI
    
          - Start button
          - Some choices with little CSS Animation

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ function getComputerChoice() {
   return Math.floor(Math.random() * 3) + 1;
 }
 
-function getUserChoice() {
-  userChoice = prompt("Enter your choice(Rock, Paper or Scissor):__") 
+function getUserChoice(userChoice) {
   switch(userChoice.toLowerCase()) {
     case 'rock':
       return 1;
@@ -11,12 +10,8 @@ function getUserChoice() {
     case 'paper':
       return 2;
       break;
-    case 'scissor': 
+    default: 
       return 3;
-      break;
-    default:
-      console.warn('Invalid User Input');
-      return getUserChoice();
   }
 }
 
@@ -30,38 +25,70 @@ function playRound(user, computer) {
   }
 }
 
-function game(){
-  let u_score = 0;
-  let c_score = 0; 
-  let draw = 0;
+const btn = document.querySelector('.start-btn');
+const choices = document.querySelector('.choices');
+const results = document.querySelector('.rps-result');
+const roundBlk = document.querySelector('.rps-rd');
+const winner = document.querySelector('.winner-announcement');
+const uscore_element = document.querySelector('.user-score strong');
+const cscore_element = document.querySelector('.computer-score strong');
+const finalresBlk = document.querySelector('.results');
+const mainBlk = document.querySelector('main');
+const res_img = document.querySelector('.results img');
+const res_txt = document.querySelector('.results .rs-title');
+let uscore = 0;
+let cscore = 0;
 
-  for (let i = 0; i < 5; i++) {
-    res = playRound(getUserChoice(), getComputerChoice());
-    //diplay winner
-    //store score
-    if (res == 0) {
-      console.info('Round is DRAW!');
-      draw++;
-    } else if ( res == 1 ) {
-      console.info(`User Won RD ${i + 1}`);
-      u_score++;
-    } else {
-      console.info(`Computer Won RD ${i + 1}`);
-      c_score++;
-    }
-  }
-  console.info(`User Score is ${u_score}`);
-  console.info(`Computer Score is ${c_score}`);
-  console.info(`DRAW: ${draw}`);
-  // declare winner
-  points = u_score - c_score;
-  if (points == 0) {
-    console.log('YOU ARE DRAW WITH AI');
-  } else if (points > 1) {
-    console.log(`YOU WON BY ${points} POINT! CONGRATULATIONS!`);
-  } else {
-    console.log(`YOU LOST BY ${points * -1} POINT! BETTER LUCK NEXT TIME.`);
-  }
-}
+btn.addEventListener('click', (e) => {
 
-game();
+
+  //hide self
+  btn.classList.toggle('hide');
+  //show choices and results block
+  choices.classList.toggle('hide');
+  results.classList.toggle('hide');
+  // Display Round 1
+  let roundhtml = "<h3>Round <strong class='rd-number'>1</strong>! Your move</h3>"
+  roundBlk.innerHTML = roundhtml;
+  const rdnum = document.querySelector('.rd-number');
+
+  //Add listener to every choice
+  for(const choice of choices.children) {
+    choice.addEventListener('click', (e) => {
+      // update round number
+      rdnum.textContent = (parseInt(rdnum.innerHTML) + 1);
+      // compare choices
+      let res = playRound(getUserChoice(choice.className.toString()), getComputerChoice());
+      
+      if(res == 1) {
+        winner.textContent = 'YOU WON! Nice';
+        uscore_element.textContent = ++uscore;
+      }
+      else if(res == 2) {
+        winner.textContent = 'AI WON! You Bastard';
+        cscore_element.textContent = ++cscore;
+      }
+      else {
+        winner.textContent = 'BOTH OF YOU CHOOSE THE SAME POISON!!';
+      }
+
+      // Display winner when score is 5
+      if (uscore == 5) {
+        console.log(uscore);
+        finalresBlk.classList.toggle('hide');
+        mainBlk.classList.toggle('hide');
+        res_img.setAttribute('src', 'images/won.svg');
+        res_txt.textContent = 'YOU WON! THANK YOU FOR SAVING THE WORLD!';
+      }
+      else if(cscore == 5) {
+        console.log(cscore);
+        finalresBlk.classList.toggle('hide');
+        mainBlk.classList.toggle('hide');
+        res_img.setAttribute('src', 'images/lost.svg');
+        document.querySelector('body').style = 'background-color: red !important;';
+      }
+    });
+  }
+
+
+});
